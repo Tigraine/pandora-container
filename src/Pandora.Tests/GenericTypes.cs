3a07c77129ec2ce993fad8fc877b606df0081c1f
Daniel Hölbling <tigraine@tigraine.at>
2009-06-10 17:10:33 +0200

    Added some more tests concerning generic registrations

--- src/Pandora.Tests/GenericTypes.cs
+++ src/Pandora.Tests/GenericTypes.cs
@@ -18,9 +18,9 @@ namespace Pandora.Tests
         [Fact]
         public void CanResolveSpecificGenericClass()
         {
-            store.Register(p => 
-                p.Service<GenericClass<string>>()
-                .Implementor<GenericClass<string>>());
+            store.Register(p =>
+                           p.Service<GenericClass<string>>()
+                               .Implementor<GenericClass<string>>());
 
             Assert.DoesNotThrow(() => container.Resolve<GenericClass<string>>());
         }
@@ -28,7 +28,6 @@ namespace Pandora.Tests
         [Fact]
         public void CanAutoResolveGenericTypes()
         {
-
             store.Register(
                 p => p.Generic(typeof (GenericClass<>))
                          .Implementor(typeof (GenericClass<>))
@@ -46,34 +45,62 @@ namespace Pandora.Tests
         public void RegisterNonGenericClassAsServiceThrowsArgumentException()
         {
             Assert.Throws<ArgumentException>(() => store.Register(
-                                                       p => p.Generic(typeof(IService))));
+                                                       p => p.Generic(typeof (IService))));
         }
 
         [Fact]
         public void RegisterNonGenericClassAsImplementorThrowsArgumentException()
         {
             Assert.Throws<ArgumentException>(() => store.Register(
-                                                       p => p.Generic(typeof(GenericClass<>))
-                                                       .Implementor(typeof(ClassWithNoDependencies))));
+                                                       p => p.Generic(typeof (GenericClass<>))
+                                                                .Implementor(typeof (ClassWithNoDependencies))));
         }
 
         [Fact]
         public void CanRegisterAndResolveRealGenericRequests()
         {
-            store.Register(p => 
-                p.Generic(typeof(GenericClass<>))
-                .Implementor(typeof(GenericClass<>))
-                .ForAllTypes());
+            store.Register(p =>
+                           p.Generic(typeof (GenericClass<>))
+                               .Implementor(typeof (GenericClass<>))
+                               .ForAllTypes());
 
-            Assert.DoesNotThrow(() => {
-                var resolve = container.Resolve<GenericClass<string>>();
-            });
+            Assert.DoesNotThrow(() => { var resolve = container.Resolve<GenericClass<string>>(); });
+        }
+
+        [Fact]
+        public void CanResolveRealGenericAsSubdependency()
+        {
+            store.Register(p =>
+                               {
+                                   p.Service<IService>()
+                                       .Implementor<ClassDependingOnGenericClass>();
+                                   p.Generic(typeof (GenericClass<>))
+                                       .Implementor(typeof (GenericClass<>))
+                                       .ForAllTypes();
+                               });
+            Assert.DoesNotThrow(() => container.Resolve<IService>());
+        }
+
+        [Fact]
+        public void CanResolveDependenciesOfGenericType()
+        {
+            store.Register(p =>
+                               {
+                                   p.Generic(typeof (GenericWithDependency<>))
+                                       .Implementor(typeof (GenericWithDependency<>))
+                                       .ForAllTypes();
+                                   p.Service<string>()
+                                       .Instance("Hello World");
+                               });
+
+            var result = container.Resolve<GenericWithDependency<string>>();
+            Assert.Equal("Hello World", result.Dependency);
         }
 
         [Fact]
         public void GenericTests()
         {
-            var type = typeof(GenericClass<>);
+            var type = typeof (GenericClass<>);
             var type2 = typeof (GenericClass<string>);
             var type3 = typeof (string);
 
@@ -81,12 +108,12 @@ namespace Pandora.Tests
             PrintTypeInfo(type2);
             PrintTypeInfo(type3);
         }
+
         private void PrintTypeInfo(Type type)
         {
             Console.WriteLine("Fullname: {0}", type.FullName);
             Console.WriteLine("IsGenericType: {0}", type.IsGenericType);
             Console.WriteLine("IsGenericTypeDefinition: {0}", type.IsGenericTypeDefinition);
-
             foreach (var t in type.GetGenericArguments())
             {
                 Console.WriteLine(t.Name);
